d18a580f77ef40af4391ebb34d8e9a4d561f150e
Роберт <pobept46.2002@gmail.com>
2024-07-05 19:13:34 +0500

    $ push

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ ua = UserAgent()
 #time.sleep(random.randint(5,60))
 
 url = "https://xn--80aaahf3a6anlhog.xn--p1ai/inap/likes"
-payload = {"path":"participants","id":"49477"}
+payload = {"path":"participants","id":"49481"} #49477"}
 
 
 print(requests.post(url,headers={"User-Agent":ua.random},json=payload).text)
